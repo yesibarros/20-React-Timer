e953fdad62820b7a41d7f1ce4f781b0b5aa5bbdb
yesibarros <yesibarros95@gmail.com>
2021-02-12 19:52:01 +0000

    this works

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -1,13 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import Clock from "./Clock";
 
 const Countdown = () => {
-    return (
-        <div>
-            Countdown
-        </div>
-    );
+  const [isRunning, setIsRunning] = useState(false);
+  const [buttonName, setbuttonName] = useState("Start");
+  const [assigTime, setAssigTime] = useState(Number(localStorage.getItem("MySameTime")));
+
+  
+  const handleInput = (event) => {
+    const value = event.target.value;
+    setAssigTime(value);
+    localStorage.setItem("MySameTime",assigTime-1)
+    console.log("LLEGUE");
+  };
+
+  const toggleIsRunning = () => {
+    setIsRunning(!isRunning);
+    isRunning ? setbuttonName("Pause") : setbuttonName("Start");
+  };
+
+  const time = () => {
+    setAssigTime((assigTime) => assigTime - 1);
+    localStorage.setItem("MySameTime",assigTime-1)
+    
+  };
+  
+  const TimerCero = () => {
+    setAssigTime(0);
+    setIsRunning(false);
+  };
+
+  
+  useEffect(() => {
+    let intervalID;
+    if (isRunning) intervalID = setInterval(time, 10);
+    if (assigTime<0){
+        TimerCero ()
+        clearInterval(intervalID)  
+    } 
+
+    return () => clearInterval(intervalID);
+
+  }, [isRunning,assigTime]);
+  
+
+  return (
+    <div>
+      <Clock time={assigTime} />
+      {/* <button className="status-button" onClick={time}>TIME</button> */}
+      <input type="number" onChange={handleInput} value={assigTime} ></input>
+      <button className="status-button" onClick={toggleIsRunning}>{buttonName}</button>
+      <button className="status-button" onClick={TimerCero}>Reset</button>
+    </div>
+  );
 };
 
 export default Countdown;
+
 
 
--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,32 +1,44 @@
-import React, { useState, useEffect} from 'react';
-import Clock from "./Clock"
-
+import React, { useState, useEffect } from "react";
+import Clock from "./Clock";
 
 const Timer = () => {
-    const [isRunning, setIsRunning]= useState(false)
-    const [buttonName, setbuttonName] = useState("Start")
-    const [assigTime, setAssigTime]=useState(0)
-    
-     const toggleIsRunning = () =>{
-        setIsRunning(!isRunning)
-        isRunning ? setbuttonName("Pause") : setbuttonName ("Start")
-     }
+  const [isRunning, setIsRunning] = useState(false);
+  const [buttonName, setbuttonName] = useState("Start");
+  const [assigTime, setAssigTime] = useState(0);
 
-     const time = ()=>{
-       setAssigTime(assigTime => assigTime+1)
-     }
+  
 
-     useEffect(()=>{isRunning && setInterval(time, 10)}, [isRunning])
-    
+  const toggleIsRunning = () => {
+    setIsRunning(!isRunning);
+    isRunning ? setbuttonName("Pause") : setbuttonName("Start");
+  };
 
+  const time = () => {
+    setAssigTime((assigTime) => assigTime + 1);
+  };
 
-    return (
-        <div>
-            <Clock time={assigTime} />   
-            {/* <button className="status-button" onClick={time}>TIME</button> */}
-            <button  className="status-button" onClick= {toggleIsRunning}> {buttonName} </button>
-        </div>
-    );
+  const TimerCero = () => {
+    setAssigTime(0);
+    setIsRunning(false);
+  };
+
+  
+  useEffect(() => {
+    let intervalID;
+    if (isRunning) intervalID = setInterval(time, 10);
+    return () => clearInterval(intervalID);
+
+  }, [isRunning]);
+  
+
+  return (
+    <div>
+      <Clock time={assigTime} />
+      {/* <button className="status-button" onClick={time}>TIME</button> */}
+      <button className="status-button" onClick={toggleIsRunning}>{buttonName}</button>
+      <button className="status-button" onClick={TimerCero}>Reset</button>
+    </div>
+  );
 };
 
 export default Timer;
